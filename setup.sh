40ea37ebab7ef2ce72e59b0604bb1444a001eece
Trần Phúc Hậu <hautp@gocit.vn>
2021-07-26 15:35:07 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -86,9 +86,9 @@ IP4=$(curl -4 -s icanhazip.com)
 
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
-echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
+echo "Internal ip = ${IP4}. External sub for ip6 = ${IP6}"
 
-echo "How many proxy do you want to create? Example 500"
+echo "How many proxy do you want to create? Example 100"
 read COUNT
 
 FIRST_PORT=30000
